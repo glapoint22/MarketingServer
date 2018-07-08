7b7f2e42f70d6f1cc684b1d1674822afe8887e24
Gabe LaPoint <glapoint22@gmail.com>
2018-07-08 13:25:52 -0400

    Updated mail controller

--- MarketingServer/Controllers/MailController.cs
+++ MarketingServer/Controllers/MailController.cs
@@ -10,6 +10,54 @@ namespace MarketingServer.Controllers
     {
         private MarketingEntities db = new MarketingEntities();
 
+        public async Task<IHttpActionResult> GetMail()
+        {
+            var categories = await db.Categories
+                .OrderBy(x => x.Name)
+                .Select(x => new
+                {
+                    id = x.ID,
+                    name = x.Name,
+                    niches = x.Niches
+                        .Select(z => new
+                        {
+                            id = z.ID,
+                            name = z.Name,
+                            emails = z.LeadMagnetEmails
+                                .Where(a => a.NicheID == z.ID)
+                                .Select(a => new {
+                                    id = a.ID,
+                                    subject = a.Subject,
+                                    body = a.Body
+                                })
+                                .ToList(),
+                            products = z.Products
+                                .Select(p => new
+                                {
+                                    id = p.ID,
+                                    name = p.Name,
+                                    emails = p.EmailCampaigns
+                                        .OrderBy(a => a.Day)
+                                        .Where(a => a.ProductID == p.ID)
+                                        .Select(a => new {
+                                            id = a.ID,
+                                            day = a.Day,
+                                            subject = a.Subject,
+                                            body = a.Body
+                                        })
+                                        .ToList()
+                                })
+                                .ToList()
+                        }).ToList()
+                }
+            )
+            .ToListAsync();
+
+            return Ok(categories);
+        }
+
+
+
         public async Task<IHttpActionResult> GetMail(string emailId, string customerId)
         {
             string emailBody = await db.EmailCampaigns.Where(e => e.ID == emailId).Select(e => e.Body).FirstOrDefaultAsync();
