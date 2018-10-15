52bf6069da83141e686ca7637bdd9ffe80c7a68c
Gabe LaPoint <glapoint22@gmail.com>
2018-10-15 19:48:42 -0400

    Updated mail controller

--- MarketingServer/Controllers/MailController.cs
+++ MarketingServer/Controllers/MailController.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace MarketingServer.Controllers
 {
@@ -44,7 +46,8 @@ namespace MarketingServer.Controllers
                                         .Select(a => new {
                                             id = a.ID,
                                             subject = a.Subject,
-                                            body = a.Body
+                                            body = a.Body,
+                                            day = a.Day
                                         })
                                         .ToList()
                                 })
@@ -72,6 +75,58 @@ namespace MarketingServer.Controllers
 
             Mail mail = new Mail(emailId, await db.Customers.Where(c => c.ID == customerId).Select(c => c).SingleAsync(), "", emailBody);
             return Ok(mail.body);
+        }
+
+
+        // PUT: api/Products/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutProduct(Product[] products)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            foreach (Product product in products)
+            {
+                // Get a list of email campaigns for this product
+                List<EmailCampaign> dbEmailCampaigns = await db.EmailCampaigns.Where(x => x.ProductID == product.ID).ToListAsync();
+
+                // Check to see if any emails have been deleted
+                foreach (EmailCampaign dbEmailCampaign in dbEmailCampaigns)
+                {
+                    if (!product.EmailCampaigns.Select(x => x.ID).ToList().Contains(dbEmailCampaign.ID))
+                    {
+                        db.EmailCampaigns.Remove(dbEmailCampaign);
+                        
+                    }
+                }
+
+                // Check to see if any email campaigns need to be added or have been modified
+                foreach (EmailCampaign emailCampaign in product.EmailCampaigns)
+                {
+                    if (!(dbEmailCampaigns.Count(x => x.ID == emailCampaign.ID) > 0))
+                    {
+                        db.Entry(emailCampaign).State = EntityState.Added;
+                    }
+                    else
+                    {
+                        EmailCampaign dbEmailCampaign = dbEmailCampaigns.FirstOrDefault(x => x.ID == emailCampaign.ID);
+                        db.Entry(dbEmailCampaign).State = EntityState.Detached;
+                        if (dbEmailCampaign.Subject != emailCampaign.Subject || dbEmailCampaign.Body != emailCampaign.Body)
+                        {
+                            db.Entry(emailCampaign).State = EntityState.Modified;
+                        }
+                    }
+                }
+            }
+
+            if (db.ChangeTracker.HasChanges())
+            {
+                await db.SaveChangesAsync();
+            }
+
+            return Ok();
         }
     }
 }
